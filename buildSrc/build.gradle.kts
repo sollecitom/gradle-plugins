2d243b7874eb2095d97a83434c41de338d3c3aaa
michele <michele.sollecito@gmail.com>
2025-04-20 23:07:32 +0100

    first commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `kotlin-dsl`
+    alias(libs.plugins.com.github.ben.manes.versions)
+    alias(libs.plugins.nl.littlerobots.version.catalog.update)
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(libs.semver4j)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.nl.littlerobots.version.catalog.update)
+    implementation(libs.com.github.ben.manes.versions)
+    implementation(libs.jib.gradle.plugin)
+}
---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "kotlin-monorepo-monolith-example"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
