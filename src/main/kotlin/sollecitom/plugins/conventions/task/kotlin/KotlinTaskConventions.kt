275a4764ceddf08fada36b2d32ef97c8e1567705
michele <michele.sollecito@gmail.com>
2025-04-21 15:53:40 +0100

    WIP

--- src/main/kotlin/sollecitom/plugins/conventions/task/kotlin/KotlinTaskConventions.kt
+++ src/main/kotlin/sollecitom/plugins/conventions/task/kotlin/KotlinTaskConventions.kt
@@ -24,7 +24,7 @@ abstract class KotlinTaskConventions : Plugin<Project> {
     companion object {
         private val optIns = emptyList<String>()
         private val optInCompilerArguments = optIns.map { "-opt-in=$it" }
-        private val compilerArgs = optInCompilerArguments + listOf("-Xcontext-receivers", "-Xjsr305=strict", "-Xwarning-level=CONTEXT_RECEIVERS_DEPRECATED:disabled")
+        private val compilerArgs = optInCompilerArguments + listOf("-Xcontext-parameters", "-Xjsr305=strict", "-Xwarning-level=CONTEXT_RECEIVERS_DEPRECATED:disabled")
         private val targetJvmVersion = JvmTarget.JVM_23
     }
 }
