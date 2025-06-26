14b1103e8a8f80ef9402726bd5128530481c8336
michele <michele.sollecito@gmail.com>
2025-06-26 23:45:16 +0100

    New module structure

--- src/main/kotlin/sollecitom/plugins/conventions/task/maven/publish/MavenPublishConvention.kt
+++ src/main/kotlin/sollecitom/plugins/conventions/task/maven/publish/MavenPublishConvention.kt
@@ -1,18 +1,28 @@
 package sollecitom.plugins.conventions.task.maven.publish
 
-import sollecitom.plugins.RepositoryConfiguration
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByName
+import sollecitom.plugins.RepositoryConfiguration
 
 abstract class MavenPublishConvention : Plugin<Project> {
 
     override fun apply(project: Project) = with(project) {
+
+        val sourceSets = properties["sourceSets"] as SourceSetContainer
+        val sourceSet = sourceSets.getByName("main")
+        val sourcesJar = tasks.register("source-jar", Jar::class.java) {
+            archiveClassifier.set("sources")
+            from(sourceSet.allSource)
+        }
 
         pluginManager.apply(MavenPublishPlugin::class)
 
@@ -27,6 +37,7 @@ abstract class MavenPublishConvention : Plugin<Project> {
                         artifactId = project.name
                         version = project.version.toString()
                         from(components["java"])
+                        artifact(sourcesJar)
                         logger.quiet("Created publication ${groupId}:${artifactId}:${version}")
                     }
                 }
