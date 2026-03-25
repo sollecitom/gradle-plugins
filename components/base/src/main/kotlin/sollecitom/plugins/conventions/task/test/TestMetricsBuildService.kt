26d8576834c62842a85ac580baa1e9ba67e8d81f
Michele Sollecito <msollecito@meta.com>
2026-03-25 21:19:28 +0000

    WIP

---
+++ components/base/src/main/kotlin/sollecitom/plugins/conventions/task/test/TestMetricsBuildService.kt
@@ -0,0 +1,38 @@
+package sollecitom.plugins.conventions.task.test
+
+import org.gradle.api.provider.Property
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+import java.util.concurrent.atomic.AtomicLong
+
+abstract class TestMetricsBuildService : BuildService<TestMetricsBuildService.Params>, AutoCloseable {
+
+    interface Params : BuildServiceParameters {
+        val projectName: Property<String>
+    }
+
+    private val testCount = AtomicLong(0L)
+    private val successfulTestCount = AtomicLong(0L)
+    private val failedTestCount = AtomicLong(0L)
+    private val skippedTestCount = AtomicLong(0L)
+
+    fun recordResults(total: Long, successful: Long, failed: Long, skipped: Long) {
+        testCount.addAndGet(total)
+        successfulTestCount.addAndGet(successful)
+        failedTestCount.addAndGet(failed)
+        skippedTestCount.addAndGet(skipped)
+    }
+
+    override fun close() {
+        println()
+        println("> ${parameters.projectName.get()}: Aggregated Test Metrics:")
+        println("\t>   Total Tests Run:     ${testCount.get()}")
+        println("\t>   Total Tests Passed:  ${successfulTestCount.get()}")
+        println("\t>   Total Tests Failed:  ${failedTestCount.get()}")
+        println("\t>   Total Tests Skipped: ${skippedTestCount.get()}")
+    }
+
+    companion object {
+        const val SERVICE_NAME = "testMetricsBuildService"
+    }
+}
