cd097fef2d5a15f94e792ee172de2c4dcc60275e
Michele Sollecito <msollecito@meta.com>
2026-03-27 16:32:01 +0000

    WIP

--- components/base/build.gradle.kts
+++ components/base/build.gradle.kts
@@ -3,3 +3,36 @@ dependencies {
     api(libs.nl.littlerobots.version.catalog.update)
     api(libs.jib.gradle.plugin)
 }
+
+gradlePlugin {
+    plugins {
+        create("dependency-update-conventions") {
+            id = "sollecitom.dependency-update-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.dependency.update.DependencyUpdateConvention"
+        }
+        create("minimum-dependency-version-conventions") {
+            id = "sollecitom.minimum-dependency-version-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.dependency.version.MinimumDependencyVersionConventions"
+        }
+        create("test-conventions") {
+            id = "sollecitom.test-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.test.TestTaskConventions"
+        }
+        create("aggregate-test-metrics-conventions") {
+            id = "sollecitom.aggregate-test-metrics-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.test.AggregateTestMetricsConventions"
+        }
+        create("maven-publish-conventions") {
+            id = "sollecitom.maven-publish-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.maven.publish.MavenPublishConvention"
+        }
+        create("jib-docker-build-conventions") {
+            id = "sollecitom.jib-docker-build-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.jib.JibDockerBuildConvention"
+        }
+        create("container-based-service-test-conventions") {
+            id = "sollecitom.container-based-service-test-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.container.test.ContainerBasedServiceTestConvention"
+        }
+    }
+}
--- components/kotlin-jvm/build.gradle.kts
+++ components/kotlin-jvm/build.gradle.kts
@@ -1,3 +1,12 @@
 dependencies {
     api(libs.kotlin.gradle.plugin)
 }
+
+gradlePlugin {
+    plugins {
+        create("kotlin-conventions") {
+            id = "sollecitom.kotlin-conventions"
+            implementationClass = "sollecitom.plugins.conventions.task.kotlin.KotlinTaskConventions"
+        }
+    }
+}
